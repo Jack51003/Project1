db5d24f06e138d9055f10169da71750f4fb3e9a0
Jack51003 <116380128+Jack51003@users.noreply.github.com>
2025-02-27 08:33:39 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,39 @@
 const numAttr = ["Value", "Value_x", "Value_y"]; // all number attributes
 let choroplethMap; // global variable for the ChoroplethMap object
+let scatterplot; // global variable for the Scatterplot object
 
 Promise.all([d3.json("data/counties-10m.json"), d3.csv("data/data.csv")])
   .then((data) => {
     const geoData = data[0];
-    const avgIncome = data[1];
+    const values = data[1];
     // take all columns names except the first one (FIPS)
-    const allAttr = Object.keys(avgIncome[0]).slice(1);
+    const allAttr = Object.keys(values[0]).slice(1);
 
     // Combine both datasets by adding the income to the TopoJSON file
     // console.log(geoData);
     geoData.objects.counties.geometries.forEach((d) => {
       // console.log(d);
-      for (let i = 0; i < avgIncome.length; i++) {
-        if (d.id === avgIncome[i].FIPS) {
+      for (let i = 0; i < values.length; i++) {
+        if (d.id === values[i].FIPS) {
           // add all attributes to the properties of the GeoJSON file
           numAttr.forEach((attr) => {
-            d.properties[attr] = +avgIncome[i][attr];
+            d.properties[attr] = +values[i][attr];
           });
           // console.log(d.properties);
         }
       }
     });
+
+    // // Scatterplot
+    // scatterplot = new Scatterplot(
+    //   {
+    //     parentElement: ".viz",
+    //   },
+    //   geoData
+    // );
+
+    // // Update scatterplot for first time
+    // scatterplot.updateVis("Value");
     // init choropleth map
     choroplethMap = new ChoroplethMap(
       {
@@ -31,15 +43,6 @@ Promise.all([d3.json("data/counties-10m.json"), d3.csv("data/data.csv")])
     );
     // update for first time
     choroplethMap.updateVis("Value");
-
-    // Bar chart
-    barChart = new BarChart(
-      {
-        parentElement: ".bar-chart",
-      },
-      datas
-    );
-    barChart.updateVis("Value");
   })
   .catch((error) => console.error(error));
 
